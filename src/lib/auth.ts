6296ac3766a543f4cffb7bc03af80a327c101166
souleymanesy7 <souleymanesyservices@gmail.com>
2025-10-23 13:55:09 +0000

    feat(better-auth): add the better-auth base configs

---
+++ src/lib/auth.ts
@@ -0,0 +1,21 @@
+import { betterAuth } from "better-auth";
+import { nextCookies } from "better-auth/next-js";
+
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { db } from "@/db";
+
+export const auth = betterAuth({
+  database: drizzleAdapter(db, {
+    provider: "pg",
+  }),
+  emailAndPassword: {
+    enabled: true,
+    requireEmailVerification: false,
+  },
+  session: {
+    expiresIn: 60 * 60 * 24 * 7, // 7 days
+    updateAge: 60 * 60 * 24, // 1 day
+  },
+  trustedOrigins: ["http://localhost:3000"],
+  plugins: [nextCookies()],
+});
