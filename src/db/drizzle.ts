808dbe810866f8134b76eecee588d2b43b97262e
souleymanesy7 <souleymanesyservices@gmail.com>
2025-10-30 20:35:44 +0000

    feat(drizzle): update the drizzle config

--- src/db/drizzle.ts
+++ src/db/drizzle.ts
@@ -1,10 +1,6 @@
 import { config } from "dotenv";
 import { drizzle } from "drizzle-orm/neon-http";
 
-import { env } from "@/utils/env";
-import * as schema from "./schema";
-
 config({ path: ".env.local" });
 
-const sql = neon(env.DATABASE_URL!);
-export const db = drizzle(sql, { schema });
+export const db = drizzle(process.env.DATABASE_URL!);
