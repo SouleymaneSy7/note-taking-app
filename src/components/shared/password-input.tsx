ef8a86e80949d3c8738394c75384f238e440200b
souleymanesy7 <souleymanesyservices@gmail.com>
2025-10-30 20:05:33 +0000

    feat(password input): update the password input component

--- src/components/shared/password-input.tsx
+++ src/components/shared/password-input.tsx
@@ -1,46 +1,56 @@
 "use client";
 
 import * as React from "react";
-import { cn } from "@/lib/utils";
 
+import { cn } from "@/lib/utils";
 import { EyeIcon, EyeOffIcon } from "lucide-react";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
+import {
+  Field,
+  FieldLabel,
+  FieldError,
+  FieldDescription,
+} from "@/components/ui/field";
 import { PasswordInputPropsType } from "@/types";
-import { FieldLabel } from "../ui/field";
-// import VisualyHidden from "../common/visualy-hidden";
 
 const PasswordInput = React.forwardRef<
   HTMLInputElement,
   PasswordInputPropsType
->(({ className, label, id, ...props }, ref) => {
+>(({ className, error, label, description, ...delegatedProps }, ref) => {
   const [showPassword, setShowPassword] = React.useState(false);
-  const inputId = id || "password";
+
+  const id = React.useId();
+  const passwordId = `password-${id}`;
 
   return (
-    <div className="flex w-full flex-col gap-3">
+    <Field data-invalid={error ? true : undefined}>
       {label && (
-        <FieldLabel htmlFor={inputId} className="font-bold">
+        <FieldLabel htmlFor={passwordId} className="font-bold">
           {label}
         </FieldLabel>
       )}
 
       <div className="relative">
         <Input
-          id={inputId}
           ref={ref}
+          id={passwordId}
+          {...delegatedProps}
           type={showPassword ? "text" : "password"}
           className={cn("pr-10", className)}
           placeholder={showPassword ? "" : "••••••••"}
-          {...props}
+          aria-invalid={error ? true : undefined}
+          aria-describedby={error ? passwordId : undefined}
         />
+
         <Button
           type="button"
           variant="ghost"
           size="icon"
           className="absolute top-0 right-0 h-full px-3 py-2 hover:bg-transparent"
           onClick={() => setShowPassword(!showPassword)}
-          aria-label={showPassword ? "Hide the password" : "Show the password"}
+          aria-label={showPassword ? "Hide password" : "Show password"}
+          tabIndex={-1}
         >
           {showPassword ? (
             <EyeOffIcon className="text-muted-foreground h-4 w-4" />
@@ -49,11 +59,15 @@ const PasswordInput = React.forwardRef<
           )}
 
           {/* <VisualyHidden> */}
-          {/*   {showPassword ? "Hide the password" : "Show the password"} */}
+          {/*   {showPassword ? "Show the password" : "Hide the password"} */}
           {/* </VisualyHidden> */}
         </Button>
       </div>
-    </div>
+
+      {description && <FieldDescription>{description}</FieldDescription>}
+
+      {error && <FieldError>{error.message}</FieldError>}
+    </Field>
   );
 });
 
