0ff80ceae48e5cbd82a21c0135f4e0b4f71f9a73
souleymanesy7 <souleymanesyservices@gmail.com>
2025-10-30 22:13:06 +0000

    feat(page): testing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,48 @@
-export default function Home() {
-  return <div>Hello World</div>;
+import Link from "next/link";
+import { headers } from "next/headers";
+
+import { auth } from "@/lib/auth";
+import { Button } from "@/components/ui/button";
+
+import Logo from "@/components/shared/logo";
+import { signOutAction } from "./actions/auth.actions";
+
+export default async function Home() {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+
+  if (!session) {
+    return (
+      <div className="m-8 flex items-center justify-between">
+        <Logo />
+
+        <div className="flex items-center gap-4">
+          <Button variant={"link"}>
+            <Link href={"/login"}>Login</Link>
+          </Button>
+
+          <Button variant={"link"}>
+            <Link href={"/signup"}>Sign Up</Link>
+          </Button>
+        </div>
+      </div>
+    );
+  }
+
+  return (
+    <div className="m-8 flex items-center justify-between">
+      <Logo />
+
+      <div className="flex flex-col gap-4">
+        <p>User Name: {session.user.name}</p>
+
+        <form action={signOutAction}>
+          <Button type="submit" size={"lg"}>
+            Logout
+          </Button>
+        </form>
+      </div>
+    </div>
+  );
 }
