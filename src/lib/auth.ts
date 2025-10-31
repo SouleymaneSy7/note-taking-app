0f24fd58a52642dc056235fd6a463c6bea5750ae
souleymanesy7 <souleymanesyservices@gmail.com>
2025-10-31 19:20:53 +0000

    feat(better-auth): update the better-auth config

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -3,10 +3,12 @@ import { nextCookies } from "better-auth/next-js";
 
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { db } from "@/db/drizzle";
+import * as schema from "@/db/schema/users.schema";
 
 export const auth = betterAuth({
   database: drizzleAdapter(db, {
     provider: "pg",
+    schema: schema,
   }),
   emailAndPassword: {
     enabled: true,
