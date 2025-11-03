cb660a42e51dcc66eb4471850293dd61b6dc54e6
souleymanesy7 <souleymanesyservices@gmail.com>
2025-11-03 15:07:22 +0000

    feat(social-provider): add the better-auth social provider for github and google

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -10,6 +10,17 @@ export const auth = betterAuth({
     provider: "pg",
     schema: userSchema,
   }),
+  socialProviders: {
+    github: {
+      clientId: process.env.GITHUB_CLIENT_ID as string,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
+    },
+    google: {
+      prompt: "select_account",
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    },
+  },
   emailAndPassword: {
     enabled: true,
     requireEmailVerification: false,
