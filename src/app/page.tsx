07aa1823403a32dc7a053328f7dffc44c517d575
souleymanesy7 <souleymanesyservices@gmail.com>
2025-11-03 08:14:39 +0000

    feat(drizzle schema): update the database schema

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,46 +1,47 @@
+import * as React from "react";
 import Link from "next/link";
+
 import { getSessionAction, signOutAction } from "./actions/auth.actions";
 
 import Logo from "@/components/shared/logo";
 import { Button } from "@/components/ui/button";
-import { toast } from "sonner";
 
 export default async function Home() {
   const session = await getSessionAction();
 
-  if (!session) {
-    return (
-      <div className="m-8 flex items-center justify-between">
-        <Logo />
-
-        <div className="flex items-center gap-4">
-          <Button variant={"link"}>
-            <Link href={"/login"}>Login</Link>
-          </Button>
-
-          <Button variant={"link"}>
-            <Link href={"/signup"}>Sign Up</Link>
-          </Button>
-        </div>
-      </div>
-    );
-  }
-
   return (
-    <div className="h-screen">
-      <div className="m-8 flex items-center justify-between">
-        <Logo />
+    <React.Fragment>
+      {session ? (
+        <div className="h-screen">
+          <div className="m-8 flex items-center justify-between">
+            <Logo />
 
-        <form action={signOutAction}>
-          <Button type="submit" size={"lg"}>
-            Logout
-          </Button>
-        </form>
-      </div>
+            <form action={signOutAction}>
+              <Button type="submit" size={"lg"}>
+                Logout
+              </Button>
+            </form>
+          </div>
 
-      <div className="flex items-center justify-center">
-        <h1 className="text-6xl">User Name: {session.user.name}</h1>
-      </div>
-    </div>
+          <div className="flex items-center justify-center">
+            <h1 className="text-6xl">User Name: {session.user.name}</h1>
+          </div>
+        </div>
+      ) : (
+        <div className="m-8 flex items-center justify-between">
+          <Logo />
+
+          <div className="flex items-center gap-4">
+            <Button variant={"link"}>
+              <Link href={"/login"}>Login</Link>
+            </Button>
+
+            <Button variant={"link"}>
+              <Link href={"/signup"}>Sign Up</Link>
+            </Button>
+          </div>
+        </div>
+      )}
+    </React.Fragment>
   );
 }
