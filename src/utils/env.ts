8bde68aff58618cb3b6ba07354b041fc3e424675
souleymanesy7 <souleymanesyservices@gmail.com>
2025-11-04 19:47:00 +0000

    chore(env validation): update the env variable validation with zod

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -1,9 +1,25 @@
-import { z } from "zod";
+import { config } from "dotenv";
+import { expand } from "dotenv-expand";
+import { z, ZodError } from "zod";
 
 const envSchema = z.object({
-  DATABASE_URL: z.string(),
-  BETTER_AUTH_SECRET: z.string(),
-  BETTER_AUTH_URL: z.string(),
+  DATABASE_URL: z.string().min(1),
+  BETTER_AUTH_URL: z.string().min(1),
+  BETTER_AUTH_SECRET: z.string().min(1),
+  GITHUB_CLIENT_ID: z.string().min(1),
+  GITHUB_CLIENT_SECRET: z.string().min(1),
+  GOOGLE_CLIENT_ID: z.string().min(1),
+  GOOGLE_CLIENT_SECRET: z.string().min(1),
 });
 
-export const env = envSchema.safeParse(process.env);
+expand(config());
+
+try {
+  envSchema.parse(process.env);
+} catch (e) {
+  if (e instanceof ZodError) {
+    console.error("Environment validation error:", e);
+  }
+}
+
+export default envSchema.parse(process.env);
