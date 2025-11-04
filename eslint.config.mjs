0f4013d8a27bdc4861e96e7464630789a01d8ab5
souleymanesy7 <souleymanesyservices@gmail.com>
2025-11-04 16:40:23 +0000

    chore(eslint config): update the eslint config to include drizzle-eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,15 +2,37 @@ import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
 
+import js from "@eslint/js";
+import typescriptParser from "@typescript-eslint/parser";
+import drizzlePlugin from "eslint-plugin-drizzle";
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
 });
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    files: ["**/*.ts", "**/*.tsx"],
+    languageOptions: {
+      parser: typescriptParser,
+      parserOptions: {
+        project: "./tsconfig.json",
+        tsconfigRootDir: __dirname,
+      },
+    },
+    plugins: {
+      drizzle: drizzlePlugin,
+    },
+    rules: {
+      "drizzle/enforce-delete-with-where": "error",
+      "drizzle/enforce-update-with-where": "error",
+    },
+  },
 ];
 
 export default eslintConfig;
