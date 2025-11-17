20bf1e18c0dda7cd63699d43c7965e403d7b8df5
souleymanesy7 <souleymanesyservices@gmail.com>
2025-11-17 17:58:27 +0000

    feat(sidebar-nav-main): update the sidebar-nav-main component styling

--- src/components/features/sidebar/sidebar-nav-main.tsx
+++ src/components/features/sidebar/sidebar-nav-main.tsx
@@ -9,6 +9,8 @@ import {
 } from "@/components/ui/sidebar";
 import { SidebarNavMainPropsType } from "@/types";
 import Link from "next/link";
+import { cn } from "@/lib/utils";
+import { ChevronRightIcon } from "lucide-react";
 
 export const SidebarNavMain: React.FC<SidebarNavMainPropsType> = ({
   items,
@@ -22,10 +24,51 @@ export const SidebarNavMain: React.FC<SidebarNavMainPropsType> = ({
 
             return (
               <SidebarMenuItem key={item.id}>
-                <SidebarMenuButton tooltip={item.title} asChild>
-                  <Link href={item.url}>
-                    {Icon && <Icon />}
-                    <span>{item.title}</span>
+                <SidebarMenuButton
+                  asChild
+                  tooltip={item.title}
+                  isActive={item.isActive}
+                >
+                  <Link
+                    href={item.url}
+                    className="flex items-center justify-between"
+                  >
+                    <div className="flex items-center gap-3">
+                      {Icon ? (
+                        <Icon
+                          className={cn(
+                            item.isActive && item.url == "/trash"
+                              ? "text-destructive"
+                              : item.isActive
+                                ? "text-primary"
+                                : undefined,
+                          )}
+                        />
+                      ) : null}
+                      <span
+                        className={cn(
+                          item.isActive && item.url == "/trash"
+                            ? "text-destructive"
+                            : item.isActive
+                              ? "text-primary"
+                              : undefined,
+                        )}
+                      >
+                        {item.title}
+                      </span>
+                    </div>
+
+                    {item.isActive ? (
+                      <ChevronRightIcon
+                        className={cn(
+                          item.isActive && item.url == "/trash"
+                            ? "text-destructive"
+                            : item.isActive
+                              ? "text-primary"
+                              : undefined,
+                        )}
+                      />
+                    ) : null}
                   </Link>
                 </SidebarMenuButton>
               </SidebarMenuItem>
