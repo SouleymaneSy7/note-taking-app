e1de5f7431598cd1c82e3b3d7e7b7d7f4b0cdb3e
souleymanesy7 <souleymanesyservices@gmail.com>
2025-11-17 17:58:58 +0000

    feat(sidebar): update the sidebar component data

--- src/components/features/sidebar/app-sidebar.tsx
+++ src/components/features/sidebar/app-sidebar.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React from "react";
 import {
   Sidebar,
@@ -16,39 +18,16 @@ import {
   TagsIcon,
   TrashIcon,
 } from "lucide-react";
+import { usePathname } from "next/navigation";
 
 const data = {
   user: {
     name: "Souleymane",
     email: "souleymane@email.com",
-    avatar: "/avatars/shadcn.jpg",
+    avatar:
+      "https://unsplash.com/fr/photos/un-homme-avec-une-barbe-debout-devant-un-fond-violet-7N5Qi3PYIBI",
   },
-  navMain: [
-    {
-      id: crypto.randomUUID(),
-      title: "Home",
-      url: "#",
-      icon: HomeIcon,
-    },
-    {
-      id: crypto.randomUUID(),
-      title: "Favorite",
-      url: "#",
-      icon: HeartIcon,
-    },
-    {
-      id: crypto.randomUUID(),
-      title: "Archive",
-      url: "#",
-      icon: ArchiveIcon,
-    },
-    {
-      id: crypto.randomUUID(),
-      title: "Trash",
-      url: "#",
-      icon: TrashIcon,
-    },
-  ],
+
   navTags: {
     tagsTitle: "Tags",
     tagsContent: [
@@ -74,20 +53,52 @@ const data = {
 const AppSidebar = ({
   ...delegatedProps
 }: React.ComponentProps<typeof Sidebar>) => {
+  const pathname = usePathname();
+  const navMain = [
+    {
+      id: crypto.randomUUID(),
+      title: "Home",
+      url: "/",
+      icon: HomeIcon,
+      isActive: pathname === "/",
+    },
+    {
+      id: crypto.randomUUID(),
+      title: "Favorite",
+      url: "/favorite",
+      icon: HeartIcon,
+      isActive: pathname === "/favorite",
+    },
+    {
+      id: crypto.randomUUID(),
+      title: "Archive",
+      url: "/archive",
+      icon: ArchiveIcon,
+      isActive: pathname === "/archive",
+    },
+    {
+      id: crypto.randomUUID(),
+      title: "Trash",
+      url: "/trash",
+      icon: TrashIcon,
+      isActive: pathname === "/trash",
+    },
+  ];
+
   return (
     <Sidebar {...delegatedProps} className="px-3">
       <SidebarNavHeader />
 
       <Separator />
       <SidebarContent>
-        <SidebarNavMain items={data.navMain} />
+        <SidebarNavMain items={navMain} />
         <Separator />
         <SidebarNavTags
           tagTitle={data.navTags.tagsTitle}
           tagsContent={data.navTags.tagsContent}
         />
       </SidebarContent>
-      <Separator className="my-8" />
+      <Separator />
 
       <SidebarFooter>
         <SidebarNavUser user={data.user} />
